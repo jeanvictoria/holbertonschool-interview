687a48207d97f6fb1fe2e4251e9e3f90d557c6f9
jeanvictoria <jean.vic8@gmail.com>
2020-07-21 10:13:19 -0500

    task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""contain canUnlockAll function"""
+
+
+def canUnlockAll(boxes):
+    """ determines if all the boxes can be opened """
+    keys = {0}
+    done = False
+
+    while not done:
+        updated = False
+        for i in range(len(boxes)):
+            if i in keys:
+                for key in boxes[i]:
+                    if key not in keys:
+                        updated = True
+                    keys.add(key)
+        done = True if not updated else False
+    return len(keys) == len(boxes)
