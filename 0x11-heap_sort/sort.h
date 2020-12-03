17631fd808a9d298e09fc2325b9eddfe01fb4706
Jean Victoria <jean.vic8@gmail.com>
2020-12-02 23:41:25 -0500

    header

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+
+#endif /* SORT_H */
