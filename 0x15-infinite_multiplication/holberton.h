0c724f564941e36a4ea762a5fef5a39b0a4493bf
Jean Victoria <jean.vic8@gmail.com>
2021-01-16 23:34:19 -0500

    header

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argcount,  char *argv[]);
+int _putchar(char c);
+unsigned int _isDigit(char *s);
+void *_calloc(long int nmemb, long int size);
+void _printstring(char *str);
+
+
+
+#endif
+
